59057e08d96c28066446501be1f7ed2f9afc02ab
Кирилл Шубарин <kirill.shubarin@gmail.com>
2020-11-06 11:35:31 +0300

    Fix redirect for view function

--- posts/views.py
+++ posts/views.py
@@ -31,6 +31,6 @@ def new_post(request):
         post = form.save(commit=False)
         post.author = request.user
         post.save()
-        return redirect(index)
+        return redirect("index")
 
-    return render(request, 'new_post.html', {'form': form})
+    return render(request, "new_post.html", {"form": form})
